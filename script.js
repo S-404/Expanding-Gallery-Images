83b273e016e1d9f22e4499a0bbd490106efe8c1c
S-404 <sudarev.andrey.i@gmail.com>
2021-09-29 21:18:37 +0300

    added script: switching images with displaying position on line

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
 const panels = document.querySelectorAll('.panel');
+const slidePosition = document.getElementById('position');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const dots = document.querySelectorAll('.dot');
 
-panels.forEach((panel) => {
+let currentPosition = 1;
+
+panels.forEach((panel, index) => {
   panel.addEventListener('click', () => {
     removeActiveClasses();
     panel.classList.add('active');
+    currentPosition = index + 1;
+    updSliderPosition();
   });
 });
+
+nextBtn.addEventListener('click', () => {
+  currentPosition++;
+  if (currentPosition > dots.length) {
+    currentPosition = dots.length;
+  }
+  console.log(currentPosition);
+  updSliderPosition();
+  changepic('next');
+});
+
+prevBtn.addEventListener('click', () => {
+  currentPosition--;
+  if (currentPosition < 1) {
+    currentPosition = 1;
+  }
+
+  updSliderPosition();
+  changepic('prev');
+});
+
+function updSliderPosition() {
+  dots.forEach((dot, index) => {
+    if (index < currentPosition) {
+      dot.classList.add('active');
+    } else {
+      dot.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.dot.active');
+  slidePosition.style.width =
+    ((actives.length - 1) / (dots.length - 1)) * 100 + '%';
+
+  if (currentPosition === 1) {
+    prevBtn.disabled = true;
+  } else if (currentPosition === dots.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
 
 function removeActiveClasses() {
   panels.forEach((panel) => {
     panel.classList.remove('active');
   });
 }
-
 function changepic(picindex) {
-  let active = document.querySelector('.active');
+  let active = document.querySelector('.panel.active');
   let target =
     picindex === 'prev'
       ? active.previousElementSibling
